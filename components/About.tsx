5215aca7feb450a91cba67ae4ff3a136c0a09a09
Ikumaru Hamanaka <m21122@g.metro-cit.ac.jp>
2025-10-07 16:42:32 +0900

    edit about

--- components/About.tsx
+++ components/About.tsx
@@ -31,8 +31,50 @@ const About = () => {
             <p className="font-semibold font-heading-4xl flex items-center space-md text-[#64ffda]">Maru</p>
           </motion.div>
 
-          {/* 右側: 自己紹介文 */}
+          {/* 自己紹介文 */}
           <motion.div
+            className="md:col-span-2 font-body-primary"
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.8, delay: 0.2, ease: 'easeInOut' }}
+          >
+            <p className="text-base font-body-primary space-sm leading-relaxed text-slate-300">
+              情報工学を専攻している学生。AIとアプリ開発を勉強しています。
+            </p>
+          </motion.div>
+
+
+          {/* 興味 */}
+          <motion.h2
+            className="md:col-span-3 font-heading-xl font-bold mb-6 text-slate-100"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.8, ease: 'easeInOut' }}
+          >
+            〜＞ ls Interests
+          </motion.h2>
+          {/* 興味のカードリスト 横並び */}
+          <motion.div
+            className="space-sm grid grid-cols-2 gap-4 mb-8"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.8, delay: 0.1, ease: 'easeInOut' }}
+          >
+            {['AI', 'App_Development', 'Game_Development', 'Teaching', 'Management', 'Volleyball'].map((interest) => (
+              <div
+                key={interest}
+                className="bg-slate-800 font-body-primary rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+              >
+                <h3 className="text-lg font-semibold text-[#64ffda] text-slate-200">{interest}</h3>
+              </div>
+            ))}
+          </motion.div>
+
+          {/* 右側: 自己紹介文 */}
+          {/* <motion.div
             className="md:col-span-2 font-body-primary"
             initial={{ opacity: 0, x: 100 }}
             whileInView={{ opacity: 1, x: 0 }}
@@ -48,7 +90,7 @@ const About = () => {
               <br />
               趣味のバレーボールで培ったチームワークも活かし、技術とコミュニケーションの両面から価値を創造できるエンジニアを目指しています。
             </p>
-          </motion.div>
+          </motion.div> */}
         </div>
       </div>
     </section>
